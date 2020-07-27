73e58e1e6968d2a01dd1bffe35897dd550cb73f3
alagarde-bit <alagarde@elon.edu>
2020-07-27 12:51:08 -0400

    NBA.com advanced passing glossary

---
+++ advanced_passes_vocabulary.R
@@ -0,0 +1,20 @@
+# advanced passing data variables explained
+
+# passes made - an attempted pass
+
+# passes received - passes as the receiver
+
+# assist - passing the ball to a teammate in a way that leads to a field goal
+
+# secondary assists - passes made by a player to a player who earned an assist. Assist must be within two seconds and one dribble 
+
+# potential assists - A pass made during assist window but the shot is missed 
+
+# AST pts created - Points created by player through their assists
+
+# AST adj - Sum of player assist, secondary assists, and free throw assists
+
+# AST TO Pass percentage - Assists divided by total passes * 100
+
+# AST TO Pass percentage adj - Total adjusted assists divided by total passes * 100
+
